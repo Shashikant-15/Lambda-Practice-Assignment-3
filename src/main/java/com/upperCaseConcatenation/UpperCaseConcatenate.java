b91565537bd1c4360979b196e63b179e933f4613
Shashikant Tanti <shashikant.tanti@gmail.com>
2021-12-29 10:00:33 +0530

    first commit

--- src/main/java/com/upperCaseConcatenation/UpperCaseConcatenate.java
+++ src/main/java/com/upperCaseConcatenation/UpperCaseConcatenate.java
@@ -3,5 +3,5 @@ package com.upperCaseConcatenation;
 @FunctionalInterface
 public interface UpperCaseConcatenate {   // interface used for UpperCaseConcatenate
 
-    String concatenate(String s1,String s2,String s3,String s4,String s5,String s6,String s7);
+    String concatenate(String string1,String string2,String string3,String string4,String string5,String string6,String string7);
 }
